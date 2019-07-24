830e6521d02a1423202b1a73d8edc68c7456b67a
amirahmadadibi <amirahmadadibi@gmail.com>
2019-07-24 12:30:36 +0430

    adding fragments dynamiclly using fragment manager

--- app/src/main/java/com/amirahmad/projects/androidfragments/MainActivity.java
+++ app/src/main/java/com/amirahmad/projects/androidfragments/MainActivity.java
@@ -1,8 +1,10 @@
 package com.amirahmad.projects.androidfragments;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FrameLayout frameLayout = findViewById(R.id.frameLayout);
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,new FooFragment());
+        fragmentTransaction.commit();
+
     }
 }
