ce687efc46c4304d7a2a53121eb2670fd2bd6a38
amirahmadadibi <amirahmadadibi@gmail.com>
2019-07-24 12:13:44 +0430

    embed single fragment

---
+++ app/src/main/java/com/amirahmad/projects/androidfragments/FooFragment.java
@@ -0,0 +1,32 @@
+package com.amirahmad.projects.androidfragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+/**
+ * Created by Amirahmad Adibi.
+ * AndroidFragments | Copyrights 2019-07-24.
+ */
+public class FooFragment extends Fragment {
+    // The onCreateView method is called when Fragment should create its View object hierarchy,
+    // either dynamically or via XML layout inflation.
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_foo,container,false);
+    }
+
+    // This event is triggered soon after onCreateView().
+    // Any view setup should occur here.  E.g., view lookups and attaching view listeners.
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        // Setup any handles to view objects here
+        // EditText etFoo = (EditText) view.findViewById(R.id.etFoo);
+    }
+}
